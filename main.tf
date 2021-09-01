6b535d74d178d9be74cf25e2af9c25cde4c103bc
Fuat YILMAZ <>
2021-09-01 10:39:42 +0200

    databricks confs are adjusted

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     databricks = {
       source  = "databrickslabs/databricks"
-      version = "0.3.5"
+      version = "0.3.7"
     }
     azurerm = {
       version = "2.72.0"
@@ -14,4 +14,18 @@ terraform {
 provider "azurerm" {
   features {}
   skip_provider_registration = true
+}
+
+provider "databricks" {
+  azure_subscription_id = ""
+  azure_client_secret   = ""
+  azure_client_id       = ""
+  azure_tenant_id       = ""
+}
+
+resource "databricks_notebook" "deploy_databricks_notebook_raw_pii_to_raw_shared" {
+  content_base64 = base64encode(templatefile(abspath("${path.module}/templates/test_file.py"), {
+  }))
+  path     = var.notebook_path
+  language = "PYTHON"
 }
--- var.tf
+++ var.tf
@@ -1,2 +1,3 @@
 variable "resource_group_name" {}
 variable "data_factory_name" {}
+variable "notebook_path" {}
